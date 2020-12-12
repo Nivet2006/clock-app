09c5e744aa7970014e62ff311d5709ce1c5094ca
Creative_minds <71724232+Creativeminds0987@users.noreply.github.com>
2020-12-12 19:24:32 +0530

    added ico

--- electron-test/index.js
+++ electron-test/index.js
@@ -8,5 +8,6 @@ function createWindow() {
     height: 150,
     resizable: true,
   });
+  mainWindow = new BrowserWindow({width: 800, height: 600,icon: 'D:\ELECTRON\electron-minds\electron-test\img-src\clock.ico'});
   win.loadFile("index.html");
 }
