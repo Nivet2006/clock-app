bb603be150f75652803c3d8e8588f1b60ef037ff
Creative_minds <71724232+Creativeminds0987@users.noreply.github.com>
2020-12-12 17:41:40 +0530

    added gulpfile.js

---
+++ electron-test/gulpfile.js
@@ -0,0 +1,17 @@
+var gulp = require('gulp');
+
+var winInstaller = require('electron-windows-installer');
+ 
+gulp.task('create-windows-installer', function (done) {
+    winInstaller({
+        appDirectory: './node_modules/electron/dist',
+        outputDirectory: './release',
+        arch: 'ia32',
+        authors: "Syncfusion",
+        version: "1.0.0",
+        iconUrl: "favicon.ico",
+        setupIcon: "favicon.ico",
+        loadingGif: "ele.gif",
+        noMsi:true
+    }).then().catch();
+});
